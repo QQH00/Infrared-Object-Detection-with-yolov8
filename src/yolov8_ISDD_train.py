96edc9d552050cc9708a570380cc67c3ced397b2
qihaoqian <2363813095@qq.com>
2024-03-26 16:28:15 +0800

    modify README and add data example

--- src/yolov8_ISDD_train.py
+++ src/yolov8_ISDD_train.py
@@ -10,4 +10,4 @@ model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data='../data/ship.yaml', device='0,1', task='detect', mode='train', epochs=500, imgsz=500, batch=-1)
+results = model.train(data='../data/ISDD.yaml', device='0,1', task='detect', mode='train', epochs=500, imgsz=500, batch=-1)
